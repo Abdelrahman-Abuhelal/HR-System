2333d5e825d44d17c8affa12f0a134eb603ebd38
abdelrahman abu hilal <AbdelrahmanA@exalt.ps>
2023-01-09 14:20:57 +0200

    third commit

--- src/main/java/com/springdata/secondDemo/exception/ApiExceptionHandler.java
+++ src/main/java/com/springdata/secondDemo/exception/ApiExceptionHandler.java
@@ -1,20 +1,26 @@
 package com.springdata.secondDemo.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {ApiRequestException.class})
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e, WebRequest webRequest) {
+
+    @ExceptionHandler(value = {NoDataFoundException.class})
+    public ResponseEntity<Object> handleApiRequestException(NoDataFoundException e, WebRequest webRequest) {
         // 1. Create payload containing exception details
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
@@ -27,4 +33,9 @@ public class ApiExceptionHandler {
         return new ResponseEntity<>(apiException, badRequest);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ApiException apiException = new ApiException(ex.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<Object>(apiException,HttpStatus.NOT_FOUND);
+    }
 }
